5ff2f7f97b5d3836af6c6b56389f82f47feb5573
Amir karta <amirkartah@gmail.com>
2025-12-07 15:09:38 +0100

    Add basic todo list functionality in web.js
    Introduces a simple todo list with add and delete features. The script renders todos with their due dates and updates the list dynamically in the DOM.

---
+++ assets/script/web.js
@@ -0,0 +1,55 @@
+const todolist = [{
+   name: 'make dinner',
+   dueDate: '2022-12-22'
+},{
+   name: 'run',
+   dueDate: '2023-12-22'
+}];
+
+renderTodoList()
+
+
+function renderTodoList(){
+   let todolistHTML = '';
+
+    for(let i = 0; i < todolist.length; i++) {
+      const todoObject = todolist[i];
+      //const name = todoObject.name
+      //const dueDate = todoObject.dueDate
+      const {name, dueDate} = todoObject;
+
+      const html = `
+         <div>${name}</div>
+         <div>${dueDate}</div>
+         <button onclick = "todolist.splice(${i}, 1);
+         renderTodoList();
+         " class="delete-button">Delete</button> 
+      
+      `;
+
+      todolistHTML += html
+    }
+    console.log(todolistHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todolistHTML
+}
+
+
+function addTodo(){
+   const inputElement = document.querySelector('.js-Tododata')
+   const name = inputElement.value
+   //console.log(name)
+   const dueDateinputElement = document.querySelector('.js-due-date-input');
+   const dueDate = dueDateinputElement.value
+
+   todolist.push({
+      name: name,
+      dueDate: dueDate
+      //name, dueDate
+   });
+   console.log(todolist);
+
+   inputElement.value = '';
+
+   renderTodoList()
+}
